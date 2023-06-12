ad08c3c649e457573063d2f7e6f89006e5cafbac
2225324 <2225324@LTIN442901>
2023-06-12 11:05:40 +0530

    Add project files.

---
+++ SQLQuery2.sql
@@ -0,0 +1,6 @@
+﻿insert into Users values('mohan@gmail.com', '6a8a7daff50dc4bb585b540e439b8dc931df708b42d6102af18c4badc58aba79',
+'Mohan', '8878053246', 0)
+go
+
+select*from Users
+go
---
+++ StackOverflowProject.ViewModels/RegisterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StackOverflowProject.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [RegularExpression(@"^[a - zA - Z0 - 9 + _.-] +@[a-zA-Z0-9.-]+$")]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]  
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Za-z][A-Za-z0-9_]{7,29}$")]
+        public string Name { get; set; }
+
+        [Required]
+        public string Mobile { get; set; }
+    }
+}
